40c168e83e4f481bed09cfd8835d26da6e943001
robertd23 <robertd23@unc.edu>
2022-04-26 11:08:59 -0400

    please3

--- server.js
+++ server.js
@@ -43,9 +43,32 @@ app.use(express.json());
 
 
 if (args.log == 'true') {
-  const logger = fs.createWriteStream("log", {flags: 'a'})
-  app.use(morgan('combined', {stream: WRITESTREAM}))
+  const logger = fs.createWriteStream("access.log", {flags: 'a'})
+  app.use(morgan('combined', {stream: logger}))
 }
+
+app.use( (req, res, next) => {
+  // Your middleware goes here.
+  let logdata = {
+    remoteaddr: req.ip,
+    remoteuser: req.user,
+    time: Date.now(),
+    method: req.method,
+    url: req.url,
+    protocol: req.protocol,
+    httpversion: req.httpVersion,
+    status: res.statusCode,
+    referer: req.headers['referer'],
+    useragent: req.headers['user-agent']
+  };
+
+  const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+  // res.status(200).json(info)
+  next()
+
+  })
+
 
 
 
