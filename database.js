2889ceabe7105ec87455a8ee9aa481318dec92e9
robertd23 <robertd23@unc.edu>
2022-05-01 19:18:52 -0400

    19

--- database.js
+++ database.js
@@ -31,7 +31,6 @@ if (row === undefined) {
             referer TEXT,
             useragent TEXT
             );
-
     `;
     db.exec(sqlInit);
     console.log('Your database has been initialized with a new table and two entries containing a username and password.');
