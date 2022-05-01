0de849786dc9ae54a17b3c4b9155123e065e3b79
robertd23 <robertd23@unc.edu>
2022-05-01 19:20:21 -0400

    minor change 25

--- database.js
+++ database.js
@@ -1,8 +1,5 @@
-
 "use strict";
 const Database = require('better-sqlite3');
-
-
 const db = new Database('log.db');
 const stmt = db.prepare(`
     SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
